5c92e814b910d1cfa7fbf834ac6e831f7471950d
Anish-Jaiswal <121757732+Anish-Jaiswal@users.noreply.github.com>
2024-07-21 23:16:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -146,9 +146,10 @@ def main():
         st.image(image, caption='Uploaded ECG Image', use_column_width=True)
 
         if st.button('Predict'):
-            # prediction, confidence = make_prediction(image)
+            prediction, 
+            # confidence = make_prediction(image)
             accuracy = 0.92  # Assuming your model accuracy is 99%
-            st.success(f'Prediction: {prediction}, Confidence: {confidence:.2f}, Accuracy: {accuracy:.2%}')
+            st.success(f'Prediction: {prediction}, Accuracy: {accuracy:.2%}')
 
 if __name__ == "__main__":
     main()
